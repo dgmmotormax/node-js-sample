c341f47e3aa68f9f3adf301b4f917d3c156fc6fa
Diego Marcos <dmarcos@motormax.com.ar>
2017-10-05 18:25:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hola babosos!')
+  response.send('Hola Hola!')
 })
 
 app.listen(app.get('port'), function() {

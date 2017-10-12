1b51224e8f96aece188290e43b1fa2aea5d6be88
Diego Marcos <dmarcos@motormax.com.ar>
2017-10-12 13:38:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Juli, el amor de mi vida!')
+  response.send('Node sample')
 })
 
 app.listen(app.get('port'), function() {
